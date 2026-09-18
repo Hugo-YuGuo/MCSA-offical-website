933f098cce7fc14e95ff05e5b6778bbe96e55d6b
Hugo-YuGuo <hugo0126@foxmail.com>
2026-09-18 20:31:31 +1000

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,5 @@
 // Local defaults. Run connect.py in the parent folder before production deployment.
 window.MCSA_CONFIG = {
-  apiBase: "http://127.0.0.1:8000/api"
+  apiBase: "" 
 };
+//http://127.0.0.1:8000/api
